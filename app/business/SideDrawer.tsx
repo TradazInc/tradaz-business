4628fbc21c7f4d5b1b06d884b024146b8df04e3d
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-06-30 14:02:03 +0100

    feat: add sidebar and drawer

---
+++ app/business/SideDrawer.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { sideBarItems } from "@/data/sideBarItems";
+import {
+  Accordion,
+  Button,
+  CloseButton,
+  Drawer,
+  HStack,
+  Icon,
+  Portal,
+} from "@chakra-ui/react";
+import { LuMenu } from "react-icons/lu";
+
+export const SideDrawer = () => {
+  return (
+    <Drawer.Root>
+      <Drawer.Trigger asChild>
+        <Button variant="outline" size="sm">
+          <LuMenu />
+        </Button>
+      </Drawer.Trigger>
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content>
+            <Drawer.Body py={20} px={0}>
+              <SideBarItems />
+            </Drawer.Body>
+            <Drawer.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Drawer.CloseTrigger>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
+  );
+};
+
+export const SideBarItems = () => {
+  return (
+    <Accordion.Root
+      collapsible
+      defaultValue={["b"]}
+      w={"full"}
+      variant={"enclosed"}
+    >
+      {sideBarItems.map((item, index) => (
+        <Accordion.Item key={index} value={item.label} my={2}>
+          <Accordion.ItemTrigger justifyContent={"space-between"}>
+            <SideBarButton>
+              <Icon fontSize="lg" color="fg.subtle">
+                <Icon as={item.icon} />
+              </Icon>
+              {item.label}
+            </SideBarButton>
+            <Accordion.ItemIndicator />
+          </Accordion.ItemTrigger>
+          {item.children &&
+            item.children.map((child, index) => (
+              <Accordion.ItemContent key={index}>
+                <Accordion.ItemBody pl={5}>
+                  <SideBarButton>
+                    <Icon fontSize="lg" color="fg.subtle">
+                      <Icon as={child.icon} />
+                    </Icon>
+                    {child.label}
+                  </SideBarButton>
+                </Accordion.ItemBody>
+              </Accordion.ItemContent>
+            ))}
+        </Accordion.Item>
+      ))}
+    </Accordion.Root>
+  );
+};
+
+export const SideBarButton = ({ children }: { children: React.ReactNode }) => {
+  return <HStack>{children}</HStack>;
+};
--- app/business/ToolBar.tsx
+++ app/business/ToolBar.tsx
@@ -1,11 +1,15 @@
 import { Button, HStack } from "@chakra-ui/react";
 import Search from "../components/Search";
 import { LuPlus } from "react-icons/lu";
+import { SideDrawer } from "./SideDrawer";
 
 const ToolBar = () => {
   return (
     <HStack w={"full"} justify={"space-between"}>
-      <Search placeholder="Search for a business" query="www" />
+      <HStack>
+        <SideDrawer />
+        <Search placeholder="Search for a business" query="www" />
+      </HStack>
       <Button size={"xs"}>
         <LuPlus />
         New Business
