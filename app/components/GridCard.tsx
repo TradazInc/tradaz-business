511f2f6afbbb64d1a40843fdf655fa14b8090e15
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-01 00:53:44 +0100

    fix: grid card

--- app/components/GridCard.tsx
+++ app/components/GridCard.tsx
@@ -1,34 +1,43 @@
-import { Box, Card, Heading, Icon, Image } from "@chakra-ui/react";
-import { LuBuilding2 } from "react-icons/lu";
+import { Card, HStack, Icon, Image, Stack, Text } from "@chakra-ui/react";
+import { LuBoxes } from "react-icons/lu";
 
 interface Props {
   logo?: string | null;
   name: string;
+  description?: string;
   address?: string;
+  createdAt?: string;
 }
 
-const GridCard = ({ logo, name, address }: Props) => {
+const GridCard = ({ logo, name, address, description, createdAt }: Props) => {
   return (
-    <Card.Root flexDirection={"row"}>
-      <Icon>
-        {logo ? (
-          <Image
-            src={logo}
-            borderRadius={"full"}
-            fit={"cover"}
+    <Card.Root size={"sm"}>
+      <Card.Body>
+        <HStack gap="3">
+          <Icon
+            size={"xl"}
             rounded={"full"}
-            as={Icon}
-          />
-        ) : (
-          <LuBuilding2 />
-        )}
-      </Icon>
-      <Box>
-        <Card.Header>
-          <Heading>{name}</Heading>
-        </Card.Header>
-        <Card.Body color={"fg.muted"}>{address}</Card.Body>
-      </Box>
+            bg={"black"}
+            contain={"content"}
+            p={1}
+          >
+            {logo ? (
+              <Image src={logo} borderRadius={"full"} fit={"cover"} />
+            ) : (
+              <LuBoxes />
+            )}
+          </Icon>
+          <Stack gap="0">
+            <Text fontWeight="semibold" textStyle="sm">
+              {name}
+            </Text>
+            <Text color="fg.muted" textStyle="sm">
+              {address} {createdAt}
+            </Text>
+          </Stack>
+        </HStack>
+        <Card.Description>{description}</Card.Description>
+      </Card.Body>
     </Card.Root>
   );
 };
--- app/components/GridContainer.tsx
+++ app/components/GridContainer.tsx
@@ -2,11 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 const GridContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SimpleGrid
-      columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }}
-      gap={6}
-      padding={{ base: 3, md: 6 }}
-    >
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }} gap={6} w={"full"}>
       {children}
     </SimpleGrid>
   );
