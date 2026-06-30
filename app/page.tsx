cc811eba171984ef6f931f288ff59b76e7f0c8cd
OgeeTech <wgift41@gmail.com>
2026-06-30 15:16:51 +0100

    feat: created dashboard  page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SigninPage from "./signin/page";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <SigninPage />
+    </div>
+  );
 };
 
 export default page;
