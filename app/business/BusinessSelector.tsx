71ee5aa2c00672d63d1dbda58b9cd2133a0d7a5e
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-06-30 01:44:53 +0100

    remove chevron

--- app/business/BusinessSelector.tsx
+++ app/business/BusinessSelector.tsx
@@ -94,7 +94,6 @@ export const BusinessSelector = () => {
                 ) : (
                   <LuChevronDown />
                 )}
-                <LuChevronDown />
               </Breadcrumb.Link>
             </BreadcrumbMenuItem>
           </>
