e61e98c6c7e837a2a61bbaeb8bbbba23caba506e
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-01 01:32:16 +0100

    fix: reduce grid gap

--- app/components/GridContainer.tsx
+++ app/components/GridContainer.tsx
@@ -2,7 +2,7 @@ import { SimpleGrid } from "@chakra-ui/react";
 
 const GridContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }} gap={6} w={"full"}>
+    <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, "2xl": 4 }} gap={4} w={"full"}>
       {children}
     </SimpleGrid>
   );
