121b1dc6a375d30a682bcf89443a775d5ca19961
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-07 16:23:51 +0100

    feat: add tast renderer

--- components/ui/provider.tsx
+++ components/ui/provider.tsx
@@ -2,11 +2,15 @@
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+import { Toaster } from "./toaster";
 
 export function Provider({ children, ...props }: ColorModeProviderProps) {
   return (
     <ColorModeProvider {...props}>
-      <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>
+      <ChakraProvider value={defaultSystem}>
+        {children}
+        <Toaster />
+      </ChakraProvider>
     </ColorModeProvider>
   );
 }
