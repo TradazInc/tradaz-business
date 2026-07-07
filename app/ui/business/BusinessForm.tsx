b87baf7184058a8c8c64727cdb65a71c7a408581
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-07 14:41:13 +0100

    feat: add business form submission

--- app/ui/business/BusinessForm.tsx
+++ app/ui/business/BusinessForm.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useBusinessCategories } from "@/hooks/businessCategory";
+import { createBusiness } from "@/services/business";
 import {
   Box,
   Button,
@@ -12,84 +13,101 @@ import {
   NativeSelect,
   Stack,
 } from "@chakra-ui/react";
+import { useTransition } from "react";
 
 export const BusinessForm = () => {
-  const { data: categories, error, isLoading } = useBusinessCategories();
+  const { data: categories, error } = useBusinessCategories();
+  const [isSubmitting, startSubmission] = useTransition();
+
+  const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    startSubmission(async () => {
+      await createBusiness(formData);
+    });
+  };
 
   if (error) return null;
 
   return (
-    <Fieldset.Root size="lg" maxW="lg">
-      <Stack>
-        <Fieldset.Legend>Business details</Fieldset.Legend>
-        <Fieldset.HelperText>
-          Please provide your business details below.
-        </Fieldset.HelperText>
-      </Stack>
+    <form onSubmit={handleSubmit}>
+      <Fieldset.Root size="lg" maxW="lg">
+        <Stack>
+          <Fieldset.Legend>Business details</Fieldset.Legend>
+          <Fieldset.HelperText>
+            Please provide your business details below.
+          </Fieldset.HelperText>
+        </Stack>
 
-      <Fieldset.Content>
-        <Field.Root required>
-          <Field.Label>
-            Name <Field.RequiredIndicator />
-          </Field.Label>
-          <Input name="name" />
-        </Field.Root>
+        <Fieldset.Content>
+          <Field.Root required>
+            <Field.Label>
+              Name <Field.RequiredIndicator />
+            </Field.Label>
+            <Input name="name" />
+          </Field.Root>
 
-        <Field.Root required>
-          <Field.Label>
-            Slug <Field.RequiredIndicator />
-          </Field.Label>
-          <Input name="slug" />
-        </Field.Root>
+          <Field.Root required>
+            <Field.Label>
+              Slug <Field.RequiredIndicator />
+            </Field.Label>
+            <Input name="slug" />
+          </Field.Root>
 
-        <Field.Root>
-          <Field.Label>Description</Field.Label>
-          <Input name="description" />
-        </Field.Root>
+          <Field.Root>
+            <Field.Label>Description</Field.Label>
+            <Input name="description" />
+          </Field.Root>
 
-        <Field.Root>
-          <Field.Label>Phone</Field.Label>
-          <Input name="phone" />
-        </Field.Root>
+          <Field.Root>
+            <Field.Label>Phone</Field.Label>
+            <Input name="phone" />
+          </Field.Root>
 
-        <Field.Root>
-          <Field.Label>Address</Field.Label>
-          <Input name="address" />
-        </Field.Root>
+          <Field.Root>
+            <Field.Label>Address</Field.Label>
+            <Input name="address" />
+          </Field.Root>
 
-        <FileUpload.Root gap="1" maxFiles={1} accept={["image/png"]}>
-          <FileUpload.HiddenInput />
-          <FileUpload.Label>Upload logo</FileUpload.Label>
-          <Input asChild>
-            <FileUpload.Trigger>
-              <FileUpload.FileText />
-            </FileUpload.Trigger>
-          </Input>
-        </FileUpload.Root>
+          <FileUpload.Root gap="1" maxFiles={1} accept={["image/png"]}>
+            <FileUpload.HiddenInput />
+            <FileUpload.Label>Upload logo</FileUpload.Label>
+            <Input asChild>
+              <FileUpload.Trigger>
+                <FileUpload.FileText />
+              </FileUpload.Trigger>
+            </Input>
+          </FileUpload.Root>
 
-        <Field.Root>
-          <Field.Label>Category</Field.Label>
-          <NativeSelect.Root>
-            <NativeSelect.Field name="category">
-              <For
-                each={categories}
-                fallback={<Box>"No categories found"</Box>}
-              >
-                {(category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                )}
-              </For>
-            </NativeSelect.Field>
-            <NativeSelect.Indicator />
-          </NativeSelect.Root>
-        </Field.Root>
-      </Fieldset.Content>
+          <Field.Root>
+            <Field.Label>Category</Field.Label>
+            <NativeSelect.Root>
+              <NativeSelect.Field name="category">
+                <For
+                  each={categories}
+                  fallback={<Box>"No categories found"</Box>}
+                >
+                  {(category) => (
+                    <option key={category.id} value={category.id}>
+                      {category.name}
+                    </option>
+                  )}
+                </For>
+              </NativeSelect.Field>
+              <NativeSelect.Indicator />
+            </NativeSelect.Root>
+          </Field.Root>
+        </Fieldset.Content>
 
-      <Button type="submit" alignSelf="flex-start">
-        Submit
-      </Button>
-    </Fieldset.Root>
+        <Button
+          type={"submit"}
+          alignSelf={"flex-start"}
+          disabled={isSubmitting}
+        >
+          Submit
+        </Button>
+      </Fieldset.Root>
+    </form>
   );
 };
