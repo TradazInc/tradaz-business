7f35ddbc0452735c3e0942d554b2d3c46aebe066
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-08 16:40:55 +0100

    fix: remove size

--- app/ui/dashboard/GridCardSkeleton.tsx
+++ app/ui/dashboard/GridCardSkeleton.tsx
@@ -2,8 +2,8 @@ import { Card, HStack, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 
 const GridCardSkeleton = () => {
   return (
-    <Card.Root h={14} w={80}>
-      <HStack width={"full"} gap={"3"}>
+    <Card.Root>
+      <HStack gap={"3"}>
         <SkeletonCircle size={"10"} />
         <SkeletonText noOfLines={2} />
       </HStack>
