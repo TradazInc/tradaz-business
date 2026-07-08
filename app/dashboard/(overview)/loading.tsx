67b1c930715acbcef5c8e833590e1c023e6a9ea2
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-08 17:00:51 +0100

    fix: add headers

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,16 +1,20 @@
 import GridCardSkeleton from "@/app/ui/dashboard/GridCardSkeleton";
 import GridContainer from "@/app/ui/dashboard/GridContainer";
 import { PageContainer } from "@/app/ui/PageContainer";
-import { For } from "@chakra-ui/react";
+import PageHeader from "@/app/ui/PageHeader";
+import { For, VStack } from "@chakra-ui/react";
 
 const loading = () => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
   return (
     <PageContainer>
-      <GridContainer>
-        <For each={skeletons}>{() => <GridCardSkeleton />}</For>
-      </GridContainer>
+      <VStack w={"full"} h={"full"}>
+        <PageHeader>Your Businesses</PageHeader>
+        <GridContainer>
+          <For each={skeletons}>{() => <GridCardSkeleton />}</For>
+        </GridContainer>
+      </VStack>
     </PageContainer>
   );
 };
--- app/ui/dashboard/GridCardSkeleton.tsx
+++ app/ui/dashboard/GridCardSkeleton.tsx
@@ -3,8 +3,8 @@ import { Card, HStack, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 const GridCardSkeleton = () => {
   return (
     <Card.Root>
-      <HStack gap={"3"} p={3}>
-        <SkeletonCircle size={"10"} />
+      <HStack gap={3} p={3}>
+        <SkeletonCircle size={10} />
         <SkeletonText noOfLines={2} />
       </HStack>
     </Card.Root>
