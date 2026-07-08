017d9493d131d770fb42a1aa64c8a89a4b3f778e
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-08 19:43:21 +0100

    feat: add form skeletons

--- app/dashboard/store/new/loading.tsx
+++ app/dashboard/store/new/loading.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { Center, For, SkeletonText, VStack } from "@chakra-ui/react";
 
 const loading = () => {
-  return (
-    <div>loading</div>
-  )
-}
+  const skeletons = [1, 2, 3, 4];
 
-export default loading
+  return (
+    <Center h={"full"} w={"full"}>
+      <VStack
+        w={"full"}
+        maxW={{ base: "full", md: "2xl", xl: "4xl" }}
+        mx={"auto"}
+        px={{ base: 4, md: 0 }}
+        gap={10}
+      >
+        <For each={skeletons}>
+          {() => <SkeletonText noOfLines={2} gap={"4"} />}
+        </For>
+      </VStack>
+    </Center>
+  );
+};
+
+export default loading;
