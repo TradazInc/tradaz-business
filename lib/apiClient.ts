65e41b40a4dce4a7680dd5b0c98e04da02ca67ba
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-09 16:27:05 +0100

    refactor: create http method enum in api client

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -1,6 +1,14 @@
 import { baseURL } from "@/data/baseUrl";
 import { BetterFetchOption, createFetch } from "@better-fetch/fetch";
 
+enum HttpMethod {
+  Get = "GET",
+  Put = "PUT",
+  Post = "POST",
+  Patch = "PATCH",
+  Delete = "DELETE",
+}
+
 export const fetchInstance = createFetch({
   baseURL: process.env.BASE_URL ?? baseURL,
   retry: { type: "linear", attempts: 3, delay: 1000 },
@@ -11,29 +19,35 @@ export class ApiClient<T> {
   constructor(private readonly endpoint: string) {}
 
   getAll = (options?: BetterFetchOption) => {
-    return fetchInstance<T[]>(this.endpoint, options).then((res) => res.data);
+    return fetchInstance<T[]>(this.endpoint, {
+      ...options,
+      method: HttpMethod.Get,
+    }).then((res) => res.data);
   };
 
   get = (id: number | string) => {
-    return fetchInstance<T>(`${this.endpoint}/${id}`).then((res) => res.data);
-  };
-
-  post = (body: unknown) => {
-    return fetchInstance<T>(this.endpoint, { body, method: "POST" }).then(
-      (res) => res.data,
-    );
-  };
-
-  update = (id: number | string, body: unknown) => {
     return fetchInstance<T>(`${this.endpoint}/${id}`, {
-      body,
-      method: "PUT",
+      method: HttpMethod.Get,
+    }).then((res) => res.data);
+  };
+
+  post = (options: BetterFetchOption) => {
+    return fetchInstance<T>(this.endpoint, {
+      ...options,
+      method: HttpMethod.Post,
+    }).then((res) => res.data);
+  };
+
+  update = (id: number | string, options: BetterFetchOption) => {
+    return fetchInstance<T>(`${this.endpoint}/${id}`, {
+      ...options,
+      method: HttpMethod.Put,
     }).then((res) => res.data);
   };
 
   delete = (id: number | string) => {
     return fetchInstance<void>(`${this.endpoint}/${id}`, {
-      method: "DELETE",
+      method: HttpMethod.Delete,
     }).then((res) => res.data);
   };
 }
