cbd474874c8e23ca9a6c7d8440f152fc77344700
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-25 21:01:05 +0100

    fix: align items to end

--- app/ui/business/TeamVariationsField.tsx
+++ app/ui/business/TeamVariationsField.tsx
@@ -73,7 +73,7 @@ const TeamVariationsField = ({
     <Fieldset.Root invalid={!!teamVariationErrors?.root?.message}>
       <Fieldset.Legend>Store Quantity</Fieldset.Legend>
       {fields.map((field, index) => (
-        <HStack key={field.id} alignItems={"center"}>
+        <HStack key={field.id} alignItems={"end"}>
           <Field.Root required invalid={!!teamVariationErrors?.[index]?.teamId}>
             <Field.Label>
               Store <Field.RequiredIndicator />
