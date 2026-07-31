f99622e01c3291db319c924195cee19aba5a1c3d
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-07-31 11:15:05 +0100

    refactor: add comment

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -18,7 +18,8 @@ interface Props {
 
 export default async function page({ searchParams }: Props) {
   const { business, signup } = await searchParams;
-  const { data: businesses, error } = await getBusinesses(business); // deduplicated (fetched in layout)
+  // getBusiness is deduplicated (fetched in layout)
+  const { data: businesses, error } = await getBusinesses(business);
 
   if (error) return null;
 
