9ae119df8bf258b6747fe24a3b826c3b6a1fd774
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-09 00:46:57 +0100

    fix: style forbidden and unauthorized pages

--- app/unauthorized.tsx
+++ app/unauthorized.tsx
@@ -1,14 +1,22 @@
-import { Button, Heading, Stack, Text } from "@chakra-ui/react";
+import { Button, Center, Heading, Icon, Stack, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { PiWarningOctagonDuotone } from "react-icons/pi";
 
 export default function Unauthorized() {
   return (
-    <Stack minH="100dvh" align="center" justify="center" gap="4" p="6">
-      <Heading size="2xl">401 — Not signed in</Heading>
-      <Text color="fg.muted">You need to sign in to access this page.</Text>
-      <Button asChild>
-        <Link href="/signin">Go to sign in</Link>
-      </Button>
-    </Stack>
+    <Center w={"full"} h={"full"}>
+      <Stack>
+        <Icon size={"xl"} color={"bg.muted"}>
+          <PiWarningOctagonDuotone />
+        </Icon>
+        <Heading size={"md"}>401 — Not signed in</Heading>
+        <Text mb={"3"} textStyle={"sm"} color={"fg.muted"}>
+          You need to sign in to access this page.{" "}
+        </Text>
+        <Button asChild>
+          <Link href="/signin">Go to sign in</Link>
+        </Button>
+      </Stack>
+    </Center>
   );
 }
