92919a9a0b78cb1c89545169c842499dc90e359c
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-09 11:40:16 +0100

    fix: increase bottom page padding

--- components/custom/shared/PageContainer.tsx
+++ components/custom/shared/PageContainer.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 export const PageContainer = ({ children }: { children: React.ReactNode }) => (
-  <Box overflowY="auto" px={{ base: 10, md: 36 }} pb={16}>
+  <Box overflowY="auto" px={{ base: 10, md: 36 }} pb={32}>
     {children}
   </Box>
 );
