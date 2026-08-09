c03cf44d158d638b6fa6009015be20b73cefe120
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-09 11:16:46 +0100

    fix: increase bottom page margin

--- components/custom/shared/PageContainer.tsx
+++ components/custom/shared/PageContainer.tsx
@@ -1,7 +1,7 @@
 import { Box } from "@chakra-ui/react";
 
 export const PageContainer = ({ children }: { children: React.ReactNode }) => (
-  <Box overflowY="auto" px={{ base: 10, md: 36 }} mb={15}>
+  <Box overflowY="auto" px={{ base: 10, md: 36 }} mb={24}>
     {children}
   </Box>
 );
