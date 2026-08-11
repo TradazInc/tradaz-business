77ac36d3f4e03a10d5d03c97c132e14f22bf09a6
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-11 15:16:41 +0100

    fix: condition for displaying empty page

--- app/dashboard/business/[businessId]/page.tsx
+++ app/dashboard/business/[businessId]/page.tsx
@@ -43,7 +43,7 @@ export default async function page({ params }: Props) {
           </DialogBox>
         </ToolBarContainer>
 
-        {data?.teams.length ? (
+        {data?.teams.length > 0 ? (
           <StoreGrid initialStores={data.teams} businessId={businessId} />
         ) : (
           <EmptyPage
