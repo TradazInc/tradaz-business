5f3bb5fd27d4f27ee1f84bc88076693d342eda51
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-11 14:33:08 +0100

    feat: implement product page

--- app/dashboard/business/[businessId]/products/page.tsx
+++ app/dashboard/business/[businessId]/products/page.tsx
@@ -1,18 +1,17 @@
-import ProductCard from "@/components/custom/business/ProductCard";
+import ProductGrid from "@/components/custom/business/ProductGrid";
 import EmptyPage from "@/components/custom/shared/EmptyPage";
-import GridContainer from "@/components/custom/shared/GridContainer";
 import { PageContainer } from "@/components/custom/shared/PageContainer";
 import PageHeader from "@/components/custom/shared/PageHeader";
 import { getProducts } from "@/server/services/product";
-import { computePath } from "@/utilities/computePath";
-import { For, VStack } from "@chakra-ui/react";
+import { Button, VStack } from "@chakra-ui/react";
+import { LuPlus } from "react-icons/lu";
 
 interface Props {
   params: Promise<{ business: string }>;
 }
 
 export default async function page({ params }: Props) {
-  const [{ data: products, error }, { business }] = await Promise.all([
+  const [{ data, error }, { business }] = await Promise.all([
     getProducts(),
     params,
   ]);
@@ -24,26 +23,18 @@ export default async function page({ params }: Props) {
       <VStack w={"full"} h={"full"}>
         <PageHeader>Your Products</PageHeader>
 
-        {products?.data.length ? (
-          <GridContainer>
-            <For each={products.data}>
-              {(product) => (
-                <ProductCard
-                  key={product.id}
-                  name={product.name}
-                  image={product.images[0].url}
-                  description={product.description}
-                  productStatus={product.productStatus}
-                  href={`${computePath({ businessId: business })}/products/${product.id}}`}
-                />
-              )}
-            </For>
-          </GridContainer>
+        {data ? (
+          <ProductGrid businessId={business} initialProducts={data} />
         ) : (
           <EmptyPage
-            title="No products found"
-            description="Create a new product"
-          ></EmptyPage>
+            title={"No products found"}
+            description={"Create a new product"}
+          >
+            <Button>
+              <LuPlus />
+              Create Product
+            </Button>
+          </EmptyPage>
         )}
       </VStack>
     </PageContainer>
--- server/hooks/product.ts
+++ server/hooks/product.ts
@@ -1,7 +1,19 @@
 import { ProductData } from "@/schema/product";
-import { PRODUCT_KEY } from "@/utilities/cacheKeys";
+import { cursorKey, PRODUCT_KEY } from "@/utilities/cacheKeys";
 import useSWRMutation from "swr/mutation";
-import { productService } from "../entities/product";
+import { Product, productService } from "../entities/product";
+import { FetchResponse } from "../entities/fetchResponse";
+import { PAGE_SIZE } from "@/data/constants";
+import useSWRInfinite from "swr/infinite";
+
+export const useProducts = (fallbackData: FetchResponse<Product>[]) => {
+  return useSWRInfinite(
+    (pageIndex, previousPageData) =>
+      cursorKey(pageIndex, previousPageData, "products", PAGE_SIZE),
+    () => productService.getAll({ throw: true }),
+    { fallbackData },
+  );
+};
 
 export const useAddProduct = () => {
   return useSWRMutation(
