7b714d202ad268e8e723aceebff2f7c18b37ea85
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-13 17:59:09 +0100

    feat: render images returned from file upload

--- components/custom/shared/FileUpload.tsx
+++ components/custom/shared/FileUpload.tsx
@@ -1,17 +1,36 @@
 "use client";
 
-import { Button } from "@chakra-ui/react";
+import { Button, HStack, Image, VStack } from "@chakra-ui/react";
 import { CldUploadWidget } from "next-cloudinary";
+import { useState } from "react";
+
+interface CloudinaryResult {
+  url: string;
+  secure_url: string;
+  public_id: string;
+}
 
 const FileUpload = () => {
+  const [urls, setUrls] = useState<string[]>([]);
+
   return (
-    <CldUploadWidget
-      signatureEndpoint={`${process.env.NEXT_PUBLIC_BASE_URL}/api/media/upload-signature`}
-    >
-      {({ open }) => {
-        return <Button onClick={() => open()}>Upload an Image</Button>;
-      }}
-    </CldUploadWidget>
+    <VStack gapY={5}>
+      <HStack>
+        {urls.length > 0 && urls.map((url) => <Image src={url} rounded="md" />)}
+      </HStack>
+      <CldUploadWidget
+        signatureEndpoint={`${process.env.NEXT_PUBLIC_BASE_URL}/api/media/upload-signature`}
+        onSuccess={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setUrls((prev) => [...prev, info.url]);
+        }}
+      >
+        {({ open }) => {
+          return <Button onClick={() => open()}>Upload an Image</Button>;
+        }}
+      </CldUploadWidget>
+    </VStack>
   );
 };
 
