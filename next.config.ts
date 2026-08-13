a60f19fbe4d03b0f25d320272576badb5629b7f2
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-13 20:42:17 +0100

    fix: add fileupload root

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
     optimizePackageImports: ["@chakra-ui/react"],
     authInterrupts: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://tradaz-api.onrender.com/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
