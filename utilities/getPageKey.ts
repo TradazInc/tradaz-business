0c3b045b280618d7cf35278dc608f2c048bcc264
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-28 08:52:49 +0100

    refactor: move queries into page key modules

--- utilities/getPageKey.ts
+++ utilities/getPageKey.ts
@@ -1,9 +1,31 @@
 import { PAGE_SIZE } from "@/data/constants";
 import { FetchResponse } from "@/lib/apiClient";
-import { CursorQuery, cursorQuery, IndexQuery, indexQuery } from "./pageQuery";
 
+type CursorQuery = { cursor?: string; pageSize: number };
+type IndexQuery = { page: number; pageSize: number };
 type QueryParams = Record<string, string | number | undefined>;
 
+// Pagination Queries
+function cursorQuery<T>(
+  pageIndex: number,
+  previousPageData: FetchResponse<T> | null,
+  pageSize: number,
+): CursorQuery | null {
+  if (previousPageData && !previousPageData.data) return null; // reached the end
+  if (pageIndex === 0) return { pageSize }; // first page, no `previousPageData`
+  return { cursor: previousPageData?.meta?.next, pageSize };
+}
+
+function indexQuery<T>(
+  pageIndex: number,
+  previousPageData: FetchResponse<T> | null,
+  pageSize: number,
+): IndexQuery | null {
+  if (previousPageData && !previousPageData.data.length) return null; // reached the end
+  return { page: pageIndex, pageSize };
+}
+
+// Pagination Cache Keys
 export function getCursorKey(cacheKey: string, params?: QueryParams) {
   return <T>(
     pageIndex: number,
