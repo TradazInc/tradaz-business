65e9f97b7afccedd596b2540b86ec67bf4eb9661
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-08-30 15:18:27 +0100

    feat: add hooks to create and remove business category

--- hooks/businessCategory.ts
+++ hooks/businessCategory.ts
@@ -3,9 +3,13 @@ import {
   BusinessCategory,
   businessCategoryService,
 } from "@/entities/businessCategory";
+import { couponService } from "@/entities/coupons";
 import { SWRInfiniteConfig } from "@/lib/apiClient";
-import { getCursorKey } from "@/utilities/computeKey";
-import useSWRInfinite from "swr/infinite";
+import { CouponData } from "@/schema/coupon";
+import { getCursorKey, getKey } from "@/utilities/computeKey";
+import { useSWRConfig } from "swr";
+import useSWRInfinite, { unstable_serialize } from "swr/infinite";
+import useSWRMutation from "swr/mutation";
 
 export const useBusinessCategories = (
   config?: SWRInfiniteConfig<BusinessCategory>,
@@ -14,5 +18,33 @@ export const useBusinessCategories = (
     getCursorKey(BUSINESS_CATEGORY_KEY, {}),
     ([key, query]) => businessCategoryService.getAll({ query, throw: true }),
     config,
+  );
+};
+
+export const useAddCoupon = () => {
+  const { mutate } = useSWRConfig();
+
+  return useSWRMutation(
+    getKey(BUSINESS_CATEGORY_KEY),
+    (key, { arg }: { arg: CouponData }) =>
+      couponService.post({ body: arg, throw: true }),
+    {
+      onSuccess: () =>
+        mutate(unstable_serialize(getCursorKey(BUSINESS_CATEGORY_KEY, {}))),
+    },
+  );
+};
+
+export const useRemoveCoupon = () => {
+  const { mutate } = useSWRConfig();
+
+  return useSWRMutation(
+    getKey(BUSINESS_CATEGORY_KEY),
+    (key, { arg }: { arg: string }) =>
+      couponService.delete(arg, { throw: true }),
+    {
+      onSuccess: () =>
+        mutate(unstable_serialize(getCursorKey(BUSINESS_CATEGORY_KEY, {}))),
+    },
   );
 };
