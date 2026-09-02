02106f5497dd3b54926d10691f93c5a9365cbd8b
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-09-02 14:06:25 +0100

    fix: require image to member user type to prevent error

--- entities/member.ts
+++ entities/member.ts
@@ -1,3 +1,10 @@
 import { authClient } from "@/lib/authClient";
 
-export type Member = typeof authClient.$Infer.Member;
+export type Member = typeof authClient.$Infer.Member & {
+  user: {
+    id: string;
+    name: string;
+    email: string;
+    image: string | null | undefined;
+  };
+};
