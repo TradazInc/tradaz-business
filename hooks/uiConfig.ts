f8dc507eb89f7a3f29a46ea09a01595512d2eeed
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-09-04 15:55:35 +0100

    fix: cache key

--- hooks/uiConfig.ts
+++ hooks/uiConfig.ts
@@ -1,4 +1,4 @@
-import { UICONFIG_KEY } from "@/data/cacheKeys";
+import { UI_CONFIG_KEY } from "@/data/cacheKeys";
 import { UIConfig, uiConfigService } from "@/entities/uiConfig";
 import { SWRInfiniteConfig } from "@/lib/apiClient";
 import { UIConfigData } from "@/schema/uiConfig";
@@ -17,7 +17,7 @@ export const useUIConfigs = (
   const query = { organizationId, ...searchQuery(searchParams) };
 
   return useSWRInfinite(
-    getCursorKey(UICONFIG_KEY, query),
+    getCursorKey(UI_CONFIG_KEY, query),
     ([key, query]) => uiConfigService.getAll({ query, throw: true }),
     configs,
   );
@@ -27,13 +27,13 @@ export const useAddUIConfigs = (organizationId: string | undefined) => {
   const { mutate } = useSWRConfig();
 
   return useSWRMutation(
-    getScopedKey(UICONFIG_KEY, organizationId),
+    getScopedKey(UI_CONFIG_KEY, organizationId),
     (key, { arg }: { arg: UIConfigData }) =>
       uiConfigService.post({ body: arg, throw: true }),
     {
       onSuccess: () =>
         mutate(
-          unstable_serialize(getCursorKey(UICONFIG_KEY, { organizationId })),
+          unstable_serialize(getCursorKey(UI_CONFIG_KEY, { organizationId })),
         ),
     },
   );
@@ -43,13 +43,13 @@ export const useRemoveUIConfig = (organizationId: string | undefined) => {
   const { mutate } = useSWRConfig();
 
   return useSWRMutation(
-    getScopedKey(UICONFIG_KEY, organizationId),
+    getScopedKey(UI_CONFIG_KEY, organizationId),
     (key, { arg }: { arg: string }) =>
       uiConfigService.delete(arg, { throw: true }),
     {
       onSuccess: () =>
         mutate(
-          unstable_serialize(getCursorKey(UICONFIG_KEY, { organizationId })),
+          unstable_serialize(getCursorKey(UI_CONFIG_KEY, { organizationId })),
         ),
     },
   );
