9b7f0aab3cc648b6e3081e30e19434d989eda417
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-09-12 05:00:00 +0100

    fix: add schema validation

--- schema/banks.ts
+++ schema/banks.ts
@@ -3,10 +3,14 @@ import { Gateway } from "./enums";
 import { createFetchResponseSchema } from "./fetchResponse";
 
 // Get All
-export const GetAllBanksQuerySchema = z.object({
-  gateway: z.enum(Gateway, { error: "Gateway is required" }),
-  country: z.string({ error: "Country is required" }),
-});
+export const GetAllBanksQuerySchema = z
+  .object({
+    gateway: z.enum(Gateway, { error: "Gateway is required" }),
+    country: z.string().optional(),
+  })
+  .refine((ctx) => ctx.gateway === Gateway.paystack && !ctx.country, {
+    error: "Country is required",
+  });
 export type GetAllBanksQueryData = z.infer<typeof GetAllBanksQuerySchema>;
 
 export const GetAllBanksOutputSchema = createFetchResponseSchema(
