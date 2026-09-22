b2cc0b0a5a6934330686d1bcab61025100522ae2
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-09-22 15:45:54 +0100

    feat: add cart grid, card and page

---
+++ components/store/CartCard.tsx
@@ -0,0 +1,71 @@
+import {
+  Card,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  LinkOverlay,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { LuBoxes } from "react-icons/lu";
+
+interface Props {
+  logo?: string | null;
+  name: string;
+  address?: string;
+  createdAt?: string;
+  href: string;
+}
+
+const CartCard = ({ logo, name, address, createdAt, href }: Props) => {
+  return (
+    <Card.Root size={"sm"}>
+      <Card.Header>
+        <HStack gap={"1.5"}>
+          <Icon
+            p={1}
+            bg={"bg"}
+            size={"xl"}
+            rounded={"full"}
+            contain={"content"}
+          >
+            {logo ? (
+              <Image src={logo} borderRadius={"full"} fit={"cover"} />
+            ) : (
+              <LuBoxes />
+            )}
+          </Icon>
+          <Heading size={"sm"}>{name}</Heading>
+        </HStack>
+      </Card.Header>
+      <Card.Footer>
+        <VStack alignItems={"flex-start"} w={"full"}>
+          <Text
+            color={"fg.muted"}
+            textStyle={"sm"}
+            textAlign={"start"}
+            w={"full"}
+          >
+            {address}
+          </Text>
+          <Text
+            color={"fg.muted"}
+            textStyle={"sm"}
+            textAlign={"start"}
+            w={"full"}
+          >
+            {createdAt}
+          </Text>
+        </VStack>
+      </Card.Footer>
+
+      <LinkOverlay asChild>
+        <NextLink href={href} />
+      </LinkOverlay>
+    </Card.Root>
+  );
+};
+
+export default CartCard;
---
+++ components/store/CartGrid.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { computePath } from "@/utilities/computePath";
+import { For } from "@chakra-ui/react";
+import GridCard from "../shared/GridCard";
+import GridContainer from "../shared/GridContainer";
+import { Business } from "@/schema/business";
+
+interface Props {
+  initialBusinesses: Business[];
+}
+
+const CartGrid = ({ initialBusinesses }: Props) => {
+  // Implement infinite scroll
+
+  return (
+    <GridContainer pb={12}>
+      <For each={initialBusinesses}>
+        {(business) => (
+          <GridCard
+            key={business.id}
+            name={business.name}
+            logo={business.logo}
+            address={JSON.parse(business.metadata)?.address}
+            createdAt={new Date(business.createdAt).toDateString()}
+            href={computePath(business.id)}
+          />
+        )}
+      </For>
+    </GridContainer>
+  );
+};
+
+export default CartGrid;
