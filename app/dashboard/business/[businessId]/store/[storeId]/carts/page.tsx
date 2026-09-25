e3a21ca76d9c54ac47f98fc4f62646ab3fe41038
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-09-25 12:49:10 +0100

    feat: add cart page

--- app/dashboard/business/[businessId]/store/[storeId]/carts/page.tsx
+++ app/dashboard/business/[businessId]/store/[storeId]/carts/page.tsx
@@ -1,5 +1,44 @@
+import EmptyPage from "@/components/shared/EmptyPage";
 import { PageContainer } from "@/components/shared/PageContainer";
+import PageHeader from "@/components/shared/PageHeader";
+import Search from "@/components/shared/Search";
+import CartGrid from "@/components/store/CartGrid";
+import { getCarts } from "@/server/cart";
+import { HStack, Spacer, VStack } from "@chakra-ui/react";
+import { Suspense } from "react";
 
-export default function page() {
-  return <PageContainer>checkout page</PageContainer>;
+interface Props {
+  params: Promise<{ businessId?: string; storeId?: string }>;
+}
+
+export default async function page({ params }: Props) {
+  const { businessId, storeId } = await params;
+  const { data: carts, error } = await getCarts(businessId, storeId);
+
+  if (error) return error?.message;
+
+  return (
+    <PageContainer>
+      <VStack w={"full"} h={"full"}>
+        <PageHeader>Carts</PageHeader>
+
+        <HStack w={"full"}>
+          <Suspense>
+            <Search placeholder={"Search for a cart"} filterField={"name"} />
+          </Suspense>
+          <Spacer />
+        </HStack>
+
+        {carts && carts.data.length > 0 ? (
+          <CartGrid
+            initialCarts={carts}
+            businessId={businessId}
+            storeId={storeId}
+          />
+        ) : (
+          <EmptyPage title="No carts found" description="Create a cart" />
+        )}
+      </VStack>
+    </PageContainer>
+  );
 }
--- server/cart.ts
+++ server/cart.ts
@@ -1,9 +1,12 @@
 import { PAGE_SIZE } from "@/data/constants";
 import { apiClient } from "@/lib/apiClient";
 
-export async function getCarts(organizationId: string | undefined) {
+export async function getCarts(
+  organizationId: string | undefined,
+  teamId: string | undefined,
+) {
   return apiClient("@get/api/cart", {
-    query: { pageSize: PAGE_SIZE, organizationId },
+    query: { pageSize: PAGE_SIZE, organizationId, teamId },
   });
 }
 
